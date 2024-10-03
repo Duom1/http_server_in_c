9fa46baaaa0a5acbabb4b9a7b746617a793180b1
duom1 <you@example.com>
2024-10-03 09:05:25 +0300

    made the build file object oriented.. bleh

--- build.py
+++ build.py
@@ -4,150 +4,151 @@ import os
 import platform
 import subprocess
 
-sources = [
-    "main.c",
-    "bstrlib/bstrlib.c",
-]
-name = "prog"
-objs: list[str] = []
-firstArg = 1
+class Builder():
 
-bstrzip = "https://github.com/websnarf/bstrlib/archive/refs/tags/v1.0.0.zip"
-bstrzipName = "bstrlib.zip"
-bstrlibFolder = bstrzipName.replace(".zip", "")
+    def __init__(self):
+        self.sources = [
+            "main.c",
+            "bstrlib/bstrlib.c",
+        ]
+        self.name = "prog"
+        self.objs: list[str] = []
+        self.firstArg = 1
 
-normalAnsi = "\x1B[0m"
-greenAnsi = "\x1B[32m"
-redAnsi= "\x1B[31m"
+        self.bstrzip = "https://github.com/websnarf/bstrlib/archive/refs/tags/v1.0.0.zip"
+        self.bstrzipName = "bstrlib.zip"
+        self.bstrlibFolder = self.bstrzipName.replace(".zip", "")
 
-EXIT_FAILURE: int = 1
+        self.normalAnsi = "\x1B[0m"
+        self.greenAnsi = "\x1B[32m"
+        self.redAnsi= "\x1B[31m"
 
-if (platform.system() == "Linux"):
-    cc = "cc"
-    cflags = "-Wall -Wextra -pedantic -std=c99"
-    cflagsDebug = "-g"
-    cflagsRelease = "-O3 -march=native"
-    ldflags = ""
-    outTac = "-o"
-    objectExt = ".o"
-    compileTac = "-c"
-    platformTac = ""
-    rm = "rm -f"
-    rmFolder = "rm -f -r"
-    curl = "curl"
-    unzip = "unzip"
-    mv = "mv"
-elif (platform.system() == "Windows"):
-    name += ".exe"
-    cc = "cl"
-    cflags = "/W4"
-    cflagsDebug = "/Zi"
-    cflagsRelease = "/O2"
-    ldflags = "ws2_32.lib"
-    outTac = "/Fe:"
-    objectExt = ".obj"
-    compileTac = "/c"
-    platformTac = "/DPLATFORM_WINDOWS"
-    rm = "del"
-    rmFolder = rm
-    curl = "curl.exe"
-    unzip = "tar -xf"
-    mv = "move"
-else:
-    print("system type not detected")
-    sys.exit(EXIT_FAILURE)
+        self.EXIT_FAILURE: int = 1
 
-objRecompiled: bool = False
+        if (platform.system() == "Linux"):
+            self.cc = "cc"
+            self.cflags = "-Wall -Wextra -pedantic -std=c99"
+            self.cflagsDebug = "-g"
+            self.cflagsRelease = "-O3 -march=native"
+            self.ldflags = ""
+            self.outTac = "-o"
+            self.objectExt = ".o"
+            self.compileTac = "-c"
+            self.platformTac = ""
+            self.rm = "rm -f"
+            self.rmFolder = "rm -f -r"
+            self.curl = "curl"
+            self.unzip = "unzip"
+            self.mv = "mv"
+        elif (platform.system() == "Windows"):
+            self.name += ".exe"
+            self.cc = "cl"
+            self.cflags = "/W4"
+            self.cflagsDebug = "/Zi"
+            self.cflagsRelease = "/O2"
+            self.ldflags = "ws2_32.lib"
+            self.outTac = "/Fe:"
+            self.objectExt = ".obj"
+            self.compileTac = "/c"
+            self.platformTac = "/DPLATFORM_WINDOWS"
+            self.rm = "del"
+            self.rmFolder = "del"
+            self.curl = "curl.exe"
+            self.unzip = "tar -xf"
+            self.mv = "move"
+        else:
+            print("system type not detected")
+            sys.exit(self.EXIT_FAILURE)
 
-def runCommand(command: str):
-    print(f"{greenAnsi}{command}{normalAnsi}")
-    if (subprocess.run(command, shell=True).returncode != 0):
-        print(f"{redAnsi}failed command:\n{command}{normalAnsi}")
-        sys.exit(EXIT_FAILURE)
+        self.objRecompiled: bool = False
 
-def compileSources():
-    global objRecompiled
-    for i, item in enumerate(sources):
-        cfileTime = os.path.getmtime(item)
-        try:
-            ofileTime = os.path.getmtime(objs[i])
-        except FileNotFoundError:
-            ofileTime = 0
-        if (cfileTime < ofileTime):
-            continue
-        objRecompiled = True
-        command = " ".join([cc, compileTac, item, outTac, objs[i], cflags, platformTac])
-        runCommand(command);
+    def runCommand(self, command: str):
+        print(f"{self.greenAnsi}{command}{self.normalAnsi}")
+        if (subprocess.run(command, shell=True).returncode != 0):
+            print(f"{self.redAnsi}failed command:\n{command}{self.normalAnsi}")
+            sys.exit(self.EXIT_FAILURE)
 
-def link():
-    global name, outTac
-    compileSources()
-    if (objRecompiled):
-        command = " ".join([cc, " ".join(objs), outTac, name, ldflags])
-        runCommand(command);
-    else:
-        print("no linking necesary")
+    def compileSources(self):
+        global objRecompiled
+        for i, item in enumerate(self.sources):
+            cfileTime = os.path.getmtime(item)
+            try:
+                ofileTime = os.path.getmtime(self.objs[i])
+            except FileNotFoundError:
+                ofileTime = 0
+            if (cfileTime < ofileTime):
+                continue
+            objRecompiled = True
+            command = " ".join([self.cc, self.compileTac, item, self.outTac, self.objs[i], self.cflags, self.platformTac])
+            self.runCommand(command);
 
-def clean():
-    command = " ".join([rm, " ".join(objs), name])
-    runCommand(command);
+    def link(self):
+        self.compileSources()
+        if (objRecompiled):
+            command = " ".join([self.cc, " ".join(self.objs), self.outTac, self.name, self.ldflags])
+            self.runCommand(command);
+        else:
+            print("no linking necesary")
 
-def craftObjs():
-    for i in sources:
-        objs.append(i.replace(".c", objectExt))
+    def clean(self):
+        command = " ".join([self.rm, " ".join(self.objs), self.name])
+        self.runCommand(command);
 
-def debug():
-    global cflags
-    cflags += f" {cflagsDebug}"
-    link()
+    def craftObjs(self):
+        for i in self.sources:
+            self.objs.append(i.replace(".c", self.objectExt))
 
-def release():
-    global cflags
-    cflags += f" {cflagsRelease}"
-    link()
+    def debug(self):
+        self.cflags += f" {self.cflagsDebug}"
+        self.link()
 
-def default():
-    debug()
+    def release(self):
+        self.cflags += f" {self.cflagsRelease}"
+        self.link()
 
-def getbstr():
-    command = " ".join([curl, "-L", bstrzip, ">", bstrzipName])
-    runCommand(command)
-    command = " ".join([unzip, bstrzipName])
-    runCommand(command)
-    command = " ".join([rm, bstrzipName]);
-    runCommand(command)
-    command = " ".join([mv, "bstrlib-1.0.0", bstrlibFolder]);
-    runCommand(command)
+    def default(self):
+        self.debug()
 
-def rmLibs():
-    command = " ".join([rmFolder, bstrlibFolder]);
-    runCommand(command)
+    def getbstr(self):
+        command = " ".join([self.curl, "-L", self.bstrzip, ">", self.bstrzipName])
+        self.runCommand(command)
+        command = " ".join([self.unzip, self.bstrzipName])
+        self.runCommand(command)
+        command = " ".join([self.rm, self.bstrzipName]);
+        self.runCommand(command)
+        command = " ".join([self.mv, "bstrlib-1.0.0", self.bstrlibFolder]);
+        self.runCommand(command)
 
-def getLibs():
-    getbstr()
+    def rmLibs(self):
+        command = " ".join([self.rmFolder, self.bstrlibFolder]);
+        self.runCommand(command)
 
-def argumentCheck():
-    if (len(sys.argv) > 1):
-        for i in sys.argv[1:]:
-            i = i.lower()
-            if (i == "clean"):
-                clean()
-            elif (i in ["lib", "libs", "getlib", "getlibs"]):
-                getLibs()
-            elif (i in ["rmlib", "rmlibs"]):
-                rmLibs()
-            elif (i in ["default", "def"]):
-                default()
-            elif (i == "debug"):
-                debug()
-            elif (i == "release"):
-                release()
-            else:
-                print(f"unkown target: {redAnsi}{i}{normalAnsi}")
-                sys.exit(1)
-    else:
-        default()
+    def getLibs(self):
+        self.getbstr()
+
+    def argumentCheck(self):
+        if (len(sys.argv) > 1):
+            for i in sys.argv[1:]:
+                i = i.lower()
+                if (i == "clean"):
+                    self.clean()
+                elif (i in ["lib", "libs", "getlib", "getlibs"]):
+                    self.getLibs()
+                elif (i in ["rmlib", "rmlibs"]):
+                    self.rmLibs()
+                elif (i in ["default", "def"]):
+                    self.default()
+                elif (i == "debug"):
+                    self.debug()
+                elif (i == "release"):
+                    self.release()
+                else:
+                    print(f"unkown target: {self.redAnsi}{i}{self.normalAnsi}")
+                    sys.exit(1)
+        else:
+            self.default()
 
 if __name__ == "__main__":
-    craftObjs()
-    argumentCheck()
+    builder = Builder()
+    builder.craftObjs()
+    builder.argumentCheck()
