d189a65ae035283a3bbc74c1fb4132988e77fe21
Your Name <you@example.com>
2024-10-10 12:47:36 +0300

    added windows tweaks

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #ifdef _WIN32
 #define HAVE_STRUCT_TIMESPEC
 #endif
+#include <ctype.h>
 #include <errno.h>
 #include <pthread.h>
 #include <signal.h>
@@ -38,6 +39,9 @@
   }
 #endif
 
+#define bsStaticList(a)                                                        \
+  (struct tagbstring){(-__LINE__), (int)strlen(a) - 1, (unsigned char *)a};
+
 #define RN "\r\n"
 #define DRN "\r\n\r\n"
 #define DN "\n\n"
@@ -50,13 +54,15 @@
   (-2) // the thread number of other fucntions like exit_clean and handle_sigint
 #define MAIN_FUCNTION_THREAD_NUM (-1) // the thread number of main function
 #define THREAD_FREE_WAIT_MILS                                                  \
-  (100) // The time program waits to check if a thread
-        // is available
+  (100)                         // The time program waits to check if a thread
+                                // is available
+#define CONSTANT_STRING_AMT (6) // the amount of constant strings
 #define EXIT_PRINTF_ERROR                                                      \
   (-20) // error code return if the program is unable to use printf
-#define CONSTANT_STRING_AMT (6) // the amount of constant strings
-#define CONNECTION_AMT (10)     // The amount of connections given to listen()
-#define BUFFER_SIZE (1024)      // Size of the request buffer.
+#define CONNECTION_AMT (10) // The amount of connections given to listen()
+#define BUFFER_SIZE (1024)  // Size of the request buffer.
+#define MAX_BUFFER                                                             \
+  (10) // MAX_BUFFER * BUFFER_SIZE is the mazimum size of a request
 #define LOG_FILE "http-server.log" // the path for the logging file
 #define THREADS (50) // The number of threads for processing requests.
 #define PORT (8080)  // Port that the program uses.
@@ -74,7 +80,12 @@ enum Http_request_types {
   HTTP_ENUM_MAX,
 };
 
-typedef struct {
+const char *http_request_strings[HTTP_ENUM_MAX] = {
+    "GET",     "HEAD",    "POST",  "PUT",   "DELETE",
+    "CONNECT", "OPTIONS", "TRACE", "PATCH",
+};
+
+typedef struct http_request_t {
   enum Http_request_types type;
   bstring path;
   bstring version;
@@ -90,6 +101,7 @@ struct tagbstring rn_bstr;
 struct tagbstring n_bstr;
 struct tagbstring rn_bstr_dummy;
 struct tagbstring n_bstr_dummy;
+
 struct tagbstring get_bstr;
 struct tagbstring head_bstr;
 struct tagbstring post_bstr;
@@ -102,6 +114,25 @@ struct tagbstring patch_bstr;
 
 FILE *logging_file;
 bool disable_debug_log = false;
+
+int closing_sig = 0;
+bool program_should_close = false;
+
+void fprint_request_struct(http_request_t *request, FILE *file) {
+  fprintf(file, "type: %s, path: %s, version: %s",
+          http_request_strings[request->type], request->path->data,
+          request->version->data);
+  int header_len = shlen(request->headers);
+  if (!(header_len > 0)) {
+    fprintf(file, "\n");
+    return;
+  }
+  for (int i = 0; i < header_len; i++) {
+    fprintf(file, ", %s: %s", request->headers[i].key,
+            request->headers[i].value->data);
+  }
+  fprintf(file, "\n");
+}
 
 void log_generic(char *msg, char *tag, int thread_num);
 
@@ -143,7 +174,7 @@ void log_generic(char *msg, char *tag, int thread_num) {
   fflush(logging_file);
 }
 
-struct {
+struct clean_on_exit {
   bool binded_sock;
   int thread_in_use[THREADS];
 #ifdef _WIN32
@@ -174,18 +205,10 @@ void exit_clean(void) {
   fclose(logging_file);
 }
 
-int closing_sig = 0;
-bool program_should_close = false;
-
 void handle_sig(int sig) {
   closing_sig = sig;
   program_should_close = true;
 }
-
-typedef struct {
-  int socket;
-  int thread_number;
-} handle_request_args;
 
 int newline_dummy(bstring string) {
   int ret;
@@ -204,11 +227,7 @@ int newline_dummy(bstring string) {
   return 0;
 }
 
-// TODO: Create a parser that parses an http request string into http request
-// struct. Does not validate path.
-// steps:
-// 1. split first line into method, path and version.
-// 2. add headers into stb hasmap
+// Does not validate path.
 // Returns 0 if successfull.
 int parse_http_request(http_request_t *request, bstring requets_str) {
   struct bstrList *lines = NULL;
@@ -224,15 +243,25 @@ int parse_http_request(http_request_t *request, bstring requets_str) {
     bstrListDestroy(lines);
     return -1;
   }
+  for (int i = 0; i < lines->qty; i++) {
+    bstring item = lines->entry[i];
+    if (bchar(item, item->slen - 1) == '\r') {
+      btrunc(item, item->slen - 1);
+    }
+    if (bchar(item, item->slen) == '\n') {
+      btrunc(item, item->slen);
+    }
+  }
+
   struct bstrList *first_line = NULL;
-  lines = bsplit(lines->entry[0], ' ');
-  if (lines == NULL) {
+  first_line = bsplit(lines->entry[0], ' ');
+  if (first_line == NULL) {
     log_error("unable to parse request string first line: pointer is null\n",
               OTHER_FUCNTION_THREAD_NUM);
     bstrListDestroy(lines);
     return -1;
   }
-  if (lines->qty < 3) {
+  if (first_line->qty < 3) {
     log_error(
         "unable to parse request string first line: qty is leass than 3\n",
         OTHER_FUCNTION_THREAD_NUM);
@@ -240,27 +269,28 @@ int parse_http_request(http_request_t *request, bstring requets_str) {
     bstrListDestroy(first_line);
     return -1;
   }
-  // TODO: There should be a funtcin for copying strings directly but
-  // i can't seem to fin it.
-  request->path = bfromcstr((const char *)first_line->entry[1]->data);
-  request->version = bfromcstr((const char *)first_line->entry[2]->data);
-  if (binstr(first_line->entry[0], 0, &get_bstr) == 0) {
+
+  request->path = bstrcpy(first_line->entry[1]);
+  request->version = bstrcpy(first_line->entry[2]);
+
+  // propably should make a function of this in the future
+  if (binstr(first_line->entry[0], 0, &get_bstr) >= 0) {
     request->type = HTTP_GET;
-  } else if (binstr(first_line->entry[0], 0, &head_bstr) == 0) {
+  } else if (binstr(first_line->entry[0], 0, &head_bstr) >= 0) {
     request->type = HTTP_HEAD;
-  } else if (binstr(first_line->entry[0], 0, &options_bstr) == 0) {
+  } else if (binstr(first_line->entry[0], 0, &options_bstr) >= 0) {
     request->type = HTTP_OPTIONS;
-  } else if (binstr(first_line->entry[0], 0, &trace_bstr) == 0) {
+  } else if (binstr(first_line->entry[0], 0, &trace_bstr) >= 0) {
     request->type = HTTP_TRACE;
-  } else if (binstr(first_line->entry[0], 0, &put_bstr) == 0) {
+  } else if (binstr(first_line->entry[0], 0, &put_bstr) >= 0) {
     request->type = HTTP_PUT;
-  } else if (binstr(first_line->entry[0], 0, &delete_bstr) == 0) {
+  } else if (binstr(first_line->entry[0], 0, &delete_bstr) >= 0) {
     request->type = HTTP_DELETE;
-  } else if (binstr(first_line->entry[0], 0, &post_bstr) == 0) {
+  } else if (binstr(first_line->entry[0], 0, &post_bstr) >= 0) {
     request->type = HTTP_POST;
-  } else if (binstr(first_line->entry[0], 0, &patch_bstr) == 0) {
+  } else if (binstr(first_line->entry[0], 0, &patch_bstr) >= 0) {
     request->type = HTTP_PATCH;
-  } else if (binstr(first_line->entry[0], 0, &connect_bstr) == 0) {
+  } else if (binstr(first_line->entry[0], 0, &connect_bstr) >= 0) {
     request->type = HTTP_CONNECT;
   } else {
     log_error("unable to get http request method\n", OTHER_FUCNTION_THREAD_NUM);
@@ -270,17 +300,23 @@ int parse_http_request(http_request_t *request, bstring requets_str) {
   }
 
   sh_new_strdup(request->headers);
-  for (int i = 0; i < lines->qty; i++) {
+  for (int i = 1; i < lines->qty; i++) {
     struct bstrList *split_pair = NULL;
-    bsplit(lines->entry[i], ':');
+    split_pair = bsplit(lines->entry[i], ':');
     if (split_pair == NULL) {
       continue;
     }
     if (split_pair->qty < 2) {
+      bstrListDestroy(split_pair);
       continue;
     }
-    bstring value_for_map = bfromcstr((const char *)split_pair->entry[1]->data);
-    shput(request->headers, split_pair->entry[0]->data, value_for_map);
+    bstring value = bstrcpy(split_pair->entry[1]);
+    char *key = malloc(split_pair->entry[0]->slen);
+    strcpy(key, (const char *)split_pair->entry[0]->data);
+    if (isspace(bchar(value, 0))) {
+      bdelete(value, 0, 1);
+    }
+    shput(request->headers, key, value);
     bstrListDestroy(split_pair);
   }
 
@@ -289,13 +325,18 @@ int parse_http_request(http_request_t *request, bstring requets_str) {
   return 0;
 }
 
+typedef struct handle_request_args {
+  int socket;
+  int thread_number;
+} handle_request_args;
+
 void *handle_request(void *inargs) {
   handle_request_args *args = (handle_request_args *)inargs;
   clean_on_exit.thread_in_use[args->thread_number] = true;
   int ret;
 
-  bstring request = bfromcstr("");
-  if (request == NULL) {
+  bstring request_str = bfromcstr("");
+  if (request_str == NULL) {
     log_critical("could not create string for request\n", args->thread_number);
     free(inargs);
     return NULL;
@@ -304,7 +345,7 @@ void *handle_request(void *inargs) {
   char *buf = malloc(BUFFER_SIZE);
   if (buf == NULL) {
     log_critical("unable to allocate space for buffer", args->thread_number);
-    bdestroy(request);
+    bdestroy(request_str);
     free(inargs);
     return NULL;
   }
@@ -331,7 +372,7 @@ void *handle_request(void *inargs) {
     }
 
     // Concatenate the buffer to the request string.
-    ret = bcatcstr(request, buf);
+    ret = bcatcstr(request_str, buf);
     if (ret != BSTR_OK) {
       log_error("failed to concat string to request string\n",
                 args->thread_number);
@@ -340,16 +381,15 @@ void *handle_request(void *inargs) {
     // Cheking if the request has come to an end.
     // The staring position for the search is cur_it * BUFFER_SIZE
     // so that we don't read the first parts of the request multiple times
-    if ((binstr(request, cur_it * BUFFER_SIZE, &drn_bstr) > 0) ||
-        (binstr(request, cur_it * BUFFER_SIZE, &dn_bstr) > 0)) {
+    if ((binstr(request_str, cur_it * BUFFER_SIZE, &drn_bstr) > 0) ||
+        (binstr(request_str, cur_it * BUFFER_SIZE, &dn_bstr) > 0)) {
       break;
     }
 
     // Cheking if the request is longer than needed.
-    static const int max_buffer = 10;
-    if (request->slen > max_buffer * BUFFER_SIZE) {
+    if (request_str->slen > MAX_BUFFER * BUFFER_SIZE) {
       log_info("request exceeded", args->thread_number);
-      fprintf(logging_file, " %i bytes\n", max_buffer * BUFFER_SIZE);
+      fprintf(logging_file, " %i bytes\n", MAX_BUFFER * BUFFER_SIZE);
       fflush(logging_file);
       goto close_without_response;
     }
@@ -359,7 +399,7 @@ void *handle_request(void *inargs) {
 
   // making a dummy string so that it can be logged better
   bool unable_to_create_dummy_request = false;
-  bstring dummy_request = bstrcpy(request);
+  bstring dummy_request = bstrcpy(request_str);
   if (dummy_request == NULL) {
     log_error("unable to create a dummy request string\n", args->thread_number);
     unable_to_create_dummy_request = true;
@@ -369,12 +409,25 @@ void *handle_request(void *inargs) {
     unable_to_create_dummy_request = true;
   }
 
-  log_debug("request:", args->thread_number);
+  log_info("request:", args->thread_number);
   if (unable_to_create_dummy_request) {
-    fprintf(logging_file, " %s", request->data);
+    fprintf(logging_file, " %s\n", request_str->data);
   } else {
-    fprintf(logging_file, " %s", dummy_request->data);
+    fprintf(logging_file, " %s\n", dummy_request->data);
   }
+  fflush(logging_file);
+
+  bdestroy(dummy_request);
+
+  http_request_t request;
+  ret = parse_http_request(&request, request_str);
+  if (ret != 0) {
+    log_error("unable to parse request\n", args->thread_number);
+    goto close_without_response;
+  }
+
+  log_debug("request parsed: ", args->thread_number);
+  fprint_request_struct(&request, logging_file);
   fflush(logging_file);
 
   // Creating a response and sending it
@@ -388,8 +441,7 @@ void *handle_request(void *inargs) {
   if (ret < 0) {
     log_info("failed to send data\n", args->thread_number);
   }
-  log_info("sent response to client\n", args->thread_number);
-  log_debug("response: ", args->thread_number);
+  log_info("response: ", args->thread_number);
   if (newline_dummy(response) != 0) {
     log_error("unable to replace newlines with dummys\n", args->thread_number);
   }
@@ -401,7 +453,7 @@ void *handle_request(void *inargs) {
 close_without_response:
 
   free(buf);
-  bdestroy(request);
+  bdestroy(request_str);
   CLOSESOCKET(args->socket);
 
   clean_on_exit.thread_in_use[args->thread_number] = false;
@@ -477,15 +529,18 @@ int main(void) {
   n_bstr_dummy = (struct tagbstring)bsStatic("\\n");
   n_bstr = (struct tagbstring)bsStatic("\n");
   rn_bstr = (struct tagbstring)bsStatic(RN);
-  get_bstr = (struct tagbstring)bsStatic("GET");
-  head_bstr = (struct tagbstring)bsStatic("HEAD");
-  post_bstr = (struct tagbstring)bsStatic("POST");
-  put_bstr = (struct tagbstring)bsStatic("PUT");
-  delete_bstr = (struct tagbstring)bsStatic("DELETE");
-  connect_bstr = (struct tagbstring)bsStatic("CONNECT");
-  options_bstr = (struct tagbstring)bsStatic("OPTIONS");
-  trace_bstr = (struct tagbstring)bsStatic("TRACE");
-  patch_bstr = (struct tagbstring)bsStatic("PATCH");
+
+#define http_string(a) bsStaticList(http_request_strings[a])
+  get_bstr = http_string(HTTP_GET);
+  head_bstr = http_string(HTTP_HEAD);
+  post_bstr = http_string(HTTP_POST);
+  put_bstr = http_string(HTTP_PUT);
+  delete_bstr = http_string(HTTP_DELETE);
+  connect_bstr = http_string(HTTP_CONNECT);
+  options_bstr = http_string(HTTP_OPTIONS);
+  trace_bstr = http_string(HTTP_TRACE);
+  patch_bstr = http_string(HTTP_PATCH);
+#undef http_string
 
 // On windows platform it is necesary to create and initialise
 // WSA.
